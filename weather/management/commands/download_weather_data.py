7dd4f1ebc22f73c2da7b50bfa14d3d67bab4367f
Ashley Felton <ashley@ropable.com>
2016-06-23 14:53:09 +0800

    Renamed cron to download_data.

--- weather/management/commands/download_weather_data.py
+++ weather/management/commands/download_weather_data.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand
-from weather.tasks import cron
+from weather.tasks import download_data
 
 
 class Command(BaseCommand):
-    help = "Runs cron to download weather observation data"
+    help = 'Downloads weather observation data according to schedule'
 
     def handle(self, *args, **options):
-        cron()
+        download_data()
