92640e4bd32c3a6213c562a40bd515aca9bffd64
Ashley Felton <ashley@ropable.com>
2016-11-16 15:45:00 +0800

    Added station_ips and write_observation management commands to weather app.

---
+++ weather/management/commands/write_observation.py
@@ -0,0 +1,20 @@
+from dateutil import parser as dateparser
+from django.core.management.base import BaseCommand, CommandError
+from weather.models import WeatherStation
+
+
+class Command(BaseCommand):
+    help = 'Accepts a string argument ("IP::TIMESTAMP::RAW_DATA") and writes a weather observation to the database.'
+
+    def add_arguments(self, parser):
+        # Required positional argument.
+        parser.add_argument('string', type=str)
+
+    def handle(self, *args, **options):
+        try:
+            (ip, ts, data) = options['string'].split('::')
+            station = WeatherStation.objects.get(ip_address=ip)
+            obs = station.save_weather_data(data, dateparser.parse(ts))
+            self.stdout.write(self.style.SUCCESS('Recorded observation {}'.format(obs)))
+        except:
+            raise CommandError('Unable to parse observation string')
