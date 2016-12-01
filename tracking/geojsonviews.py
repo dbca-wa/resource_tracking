56d8c159b534373254cd6189c95216038ab6a68d
Ashley Felton <ashley@ropable.com>
2016-12-01 15:46:38 +0800

    Update Django to v1.10.3, refactor geojsonviews.py.

--- tracking/geojsonviews.py
+++ tracking/geojsonviews.py
@@ -1,17 +1,15 @@
-from django.conf.urls import patterns, include, url
+from datetime import timedelta, datetime
+from django.conf.urls import url
 from django.core.serializers import serialize
 from django.http import HttpResponse
 from django.views.generic import View
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator 
-from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.utils import timezone
 from django.contrib.gis.geos import LineString
 
-from datetime import timedelta,datetime
-
-from tracking.models import Device,LoggedPoint
+from tracking.models import Device, LoggedPoint
 from tracking.basic_auth import logged_in_or_basicauth
+
 
 class GeojsonView(View):
     """
@@ -36,7 +34,7 @@ class DevicesView(GeojsonView):
     """
     Process http request and return devices as geojson
     """
-    
+
     @property
     def srid(self):
         if DevicesView._srid is None:
@@ -46,12 +44,12 @@ class DevicesView(GeojsonView):
 
         return DevicesView._srid
 
-    def get(self,request):
+    def get(self, request):
         """
         generate geojson data
         """
         try:
-            days = int(request.GET.get("days",default=14))
+            days = int(request.GET.get("days", default=14))
         except:
             days = 14
 
@@ -59,12 +57,35 @@ class DevicesView(GeojsonView):
         if days:
             q = q.filter(seen__gte=timezone.now() - timedelta(days=days))
 
-        geojson = serialize('geojson',q,geometry_field='point',srid=self.srid or "4326",properties=('id','deviceid','name','callsign','symbol','rego','make','model','category','heading','velocity','altitude','seen','age_minutes','age_colour','age_text','icon'))
+        geojson = serialize(
+            'geojson',
+            q,
+            geometry_field='point',
+            srid=self.srid or "4326",
+            properties=(
+                'id',
+                'deviceid',
+                'name',
+                'callsign',
+                'symbol',
+                'rego',
+                'make',
+                'model',
+                'category',
+                'heading',
+                'velocity',
+                'altitude',
+                'seen',
+                'age_minutes',
+                'age_colour',
+                'age_text',
+                'icon'))
 
         geojson_type = ('application/vnd.geo+json', None)
-        response = HttpResponse(geojson,content_type=geojson_type)
+        response = HttpResponse(geojson, content_type=geojson_type)
 
         return response
+
 
 class LoggedPointView(GeojsonView):
     """
@@ -79,7 +100,7 @@ class LoggedPointView(GeojsonView):
 
         return LoggedPointView._srid
 
-    def get(self,request,deviceid=None):
+    def get(self, request, deviceid=None):
         """
         generate geojson data
         """
@@ -89,27 +110,28 @@ class LoggedPointView(GeojsonView):
         except:
             deviceid = None
         if deviceid:
-            start = request.GET.get("start",default=None)
+            start = request.GET.get("start", default=None)
             if start is None:
-                #start is missing, use the default value: one day before
+                # start is missing, use the default value: one day before
                 start = timezone.now() - timedelta(days=1)
             else:
                 try:
-                    #parsing the start date with the date format: iso 8601
-                    start = datetime.strptime(start,"%Y-%m-%d %H:%M:%S")
+                    # parsing the start date with the date format: iso 8601
+                    start = datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
                 except:
-                    #Failed to parse the start date, use the default value: one day before
+                    # Failed to parse the start date, use the default value:
+                    # one day before
                     start = timezone.now() - timedelta(days=1)
-                    
-            end = request.GET.get("end",default=None)
+
+            end = request.GET.get("end", default=None)
             if end is not None:
                 try:
-                    #parsing the end date with the date format: iso 8601
-                    end = datetime.strptime(end,"%Y-%m-%d %H:%M:%S")
+                    # parsing the end date with the date format: iso 8601
+                    end = datetime.strptime(end, "%Y-%m-%d %H:%M:%S")
                 except:
                     end = None
 
-            #ignore the bbox               
+            # ignore the bbox
             q = LoggedPoint.objects.filter(device_id=deviceid)
 
             if start:
@@ -118,56 +140,66 @@ class LoggedPointView(GeojsonView):
             if end:
                 q = q.filter(seen__lt=end)
 
-
             geojson = self.get_geojson(q)
-    
 
         geojson_type = ('application/vnd.geo+json', None)
-        response = HttpResponse(geojson,content_type=geojson_type)
+        response = HttpResponse(geojson, content_type=geojson_type)
 
         return response
 
     def get_geojson(q):
         return "{}"
 
+
 class HistoryView(LoggedPointView):
     """
     Process http request and return device's history as geojson
     """
-    def get_geojson(self,q):
+
+    def get_geojson(self, q):
         """
         generate geojson data
         """
-        return serialize('geojson',q,geometry_field='point',srid=self.srid or "4326" ,properties=('id','heading','velocity','altitude','seen','raw','device_id'))
+        return serialize('geojson', q, geometry_field='point', srid=self.srid or "4326", properties=(
+            'id', 'heading', 'velocity', 'altitude', 'seen', 'raw', 'device_id'))
+
 
 class RouteView(LoggedPointView):
     """
     Process http request and return device's route as geojson
     """
 
-    def get_geojson(self,q):
+    def get_geojson(self, q):
         """
         generate geojson data
         """
 
-        #add linestring spatial data and label to convert point to line.
+        # add linestring spatial data and label to convert point to line.
         start_point = None
         for p in q:
             if start_point is not None:
-                setattr(start_point,"route",LineString(start_point.point,p.point))
-                setattr(start_point,"label","{0} to {1}".format(start_point.seen.strftime("%H:%M:%S") if start_point.seen else "",p.seen.strftime("%H:%M:%S") if p.seen else ""))
-                
+                setattr(
+                    start_point, "route", LineString(
+                        start_point.point, p.point))
+                setattr(
+                    start_point,
+                    "label",
+                    "{0} to {1}".format(
+                        start_point.seen.strftime(
+                            "%H:%M:%S") if start_point.seen else "",
+                        p.seen.strftime("%H:%M:%S") if p.seen else ""))
+
             start_point = p
 
-        #exclude the last point, because the last point is not a line string.
+        # exclude the last point, because the last point is not a line string.
         q = q[:len(q) - 1]
 
+        return serialize('geojson', q, geometry_field='route', srid=self.srid or "4326", properties=(
+            'id', 'heading', 'velocity', 'altitude', 'seen', 'raw', 'device_id', 'label'))
 
-        return serialize('geojson',q,geometry_field='route',srid=self.srid or "4326" ,properties=('id','heading','velocity','altitude','seen','raw','device_id','label'))
 
-
-geojson_patterns = patterns('',
-    url(r'^devices.geojson$', DevicesView.as_view() ),
+geojson_patterns = [
+    url(r'^devices.geojson$', DevicesView.as_view()),
     url(r'^loggedpoint/(?P<deviceid>[0-9]+).geojson$', HistoryView.as_view()),
     url(r'^route/(?P<deviceid>[0-9]+).geojson$', RouteView.as_view()),
-)
+]
