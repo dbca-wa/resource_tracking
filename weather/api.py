3b6482be66580aaf5f22938c98bca65a96be9a33
Ashley Felton <ashley@ropable.com>
2017-06-27 11:34:39 +0800

    Added station_name and station_bom_abbreviation to WeatherObservation class.

--- weather/api.py
+++ weather/api.py
@@ -16,5 +16,7 @@ class WeatherObservationResource(APIResource):
     date = fields.CharField(attribute='local_date', readonly=True)
     rainfall = fields.DecimalField(attribute='actual_rainfall', readonly=True)
     dew_point = fields.DecimalField(attribute='dew_point', readonly=True)
-    station = fields.IntegerField(attribute='station_id', readonly=True)
+    station_id = fields.IntegerField(attribute='station_id', readonly=True)
+    station_name = fields.CharField(attribute='station__name', readonly=True)
+    station_bom_abbreviation = fields.CharField(attribute='station__bom_abbreviation', readonly=True)
     Meta = generate_meta(WeatherObservation)
