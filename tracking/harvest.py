e9a0a115de284fa537fe7fdba113ad94ec73c2f6
Ashley Felton <ashley@ropable.com>
2017-08-02 13:03:07 +0800

    Removed Iriditrak harvest function first if statement (legacy?)

--- tracking/harvest.py
+++ tracking/harvest.py
@@ -118,90 +118,67 @@ def save_iriditrak(dimap, queueitem):
     else:
         LOGGER.info("Can't find date in " + str(msg.__dict__))
     sbd = {'ID': deviceid, 'TU': timestamp, 'TY': 'iriditrak'}
-    # Normal BEAM sbdtext message
-    try:
-        if attachment.find(',') == 0:
-            # NOTE: the following lines will never work as the sbdfield function is
-            # note defined anywhere. Leaving it in at present for posterity's sake.
-            """
-            for field in ['SQ', 'FU', 'DD', 'LT', 'LG', 'TU', 'VL', 'DR', 'AL', 'EQ']:
-                try:
-                    sbd[field] = sbdfield(attachment, field)
-                except:
-                    pass
-            """
-            try:
-                sbd['LOCALTU'] = sbd['FU']
-                sbd['FU'] = None
-            except:
-                pass
-        # BEAM binary message, 10byte or 20byte
-        elif len(attachment) <= 20:
-            try:
-                raw = struct.unpack("<BBBBBBBBBBIHHH"[:len(attachment) + 1], attachment)
-                # Byte 1 Equation byte, use to detect type of message
-                sbd['EQ'] = raw[0]
-                # BEAM 10byte and 20byte binary messages
-                if sbd['EQ'] in [1, 2, 3, 4, 18, 19, 25, 26]:
-                    # Byte 2: SSSS:GPS:Lat:Lng:Msd (SSSS = SQ, Msd = Most Significant Digit of Longitude)
-                    sbd['SQ'] = int('0' + bin(raw[1])[2:][-8:-4], 2)
-                    #GPS = int(bin(raw[1])[2:][-4])
-                    Lat = int(bin(raw[1])[2:][-3]) * '-'
-                    Lng = int(bin(raw[1])[2:][-2]) * '-'
-                    LngH = bin(raw[1])[2:][-1]
-                    # Byte 3,4 (Latitude HHMM)
-                    LatH = str(int('0' + bin(raw[2])[2:][-4:], 2)) + str(int('0' + bin(raw[2])[2:][-8:-4], 2))
-                    LatM = str(int('0' + bin(raw[3])[2:][-4:], 2)) + str(int('0' + bin(raw[3])[2:][-8:-4], 2))
-                    # Byte 5,6 (Latitude .MMMM)
-                    LatM += '.' + str(int('0' + bin(raw[4])[2:][-4:], 2)) + str(int('0' + bin(raw[4])[2:][-8:-4], 2))
-                    LatM += str(int('0' + bin(raw[5])[2:][-4:], 2)) + str(int('0' + bin(raw[5])[2:][-8:-4], 2))
-                    sbd['LT'] = float(Lat + str(int(LatH) + float(LatM) / 60))
-                    # Byte 7,8 (Longitude HHMM)
-                    LngH += str(int('0' + bin(raw[6])[2:][-4:], 2)) + str(int('0' + bin(raw[6])[2:][-8:-4], 2))
-                    LngM = str(int('0' + bin(raw[7])[2:][-4:], 2)) + str(int('0' + bin(raw[7])[2:][-8:-4], 2))
-                    # Byte 9,10 (Longitude .MMMM)
-                    LngM += '.' + str(int('0' + bin(raw[8])[2:][-4:], 2)) + str(int('0' + bin(raw[8])[2:][-8:-4], 2))
-                    LngM += str(int('0' + bin(raw[9])[2:][-4:], 2)) + str(int('0' + bin(raw[9])[2:][-8:-4], 2))
-                    sbd['LG'] = float(Lng + str(int(LngH) + float(LngM) / 60))
-                    if not lat_long_isvalid(sbd['LT'], sbd['LG']):
-                        raise ValueError('Lon/Lat {},{} is not valid.'.format(sbd['LG'], sbd['LT']))
-                    if len(raw) == 14:
-                        # Byte 11,12,13,14 is unix time, but local to the device??
-                        # use email timestamp because within 10 secs and fairly accurate
-                        # might have future issues with delayed retransmits
-                        sbd['LOCALTU'] = raw[10]
-                        # Byte 15,16 are speed in 10 m/h
-                        sbd['VL'] = raw[11] * 10
-                        # Byte 17,18 is altitude in m above sea level
-                        sbd['AL'] = raw[12]
-                        # Byte 19,20 is direction in degrees
-                        sbd['DR'] = raw[13]
-                    LOGGER.debug(str(sbd))
-                else:
-                    LOGGER.warning("Don't know how to read " + force_text(sbd['EQ']) + " - " + force_text(raw))
-                    dimap.flag(msgid)
-                    return
-            except Exception as e:
-                LOGGER.warning("error: " + force_text(e))
+    # BEAM binary message, 10byte or 20byte
+    if len(attachment) <= 20:
+        try:
+            raw = struct.unpack("<BBBBBBBBBBIHHH"[:len(attachment) + 1], attachment)
+            # Byte 1 Equation byte, use to detect type of message
+            sbd['EQ'] = raw[0]
+            # BEAM 10byte and 20byte binary messages
+            if sbd['EQ'] in [1, 2, 3, 4, 18, 19, 25, 26]:
+                # Byte 2: SSSS:GPS:Lat:Lng:Msd (SSSS = SQ, Msd = Most Significant Digit of Longitude)
+                sbd['SQ'] = int('0' + bin(raw[1])[2:][-8:-4], 2)
+                Lat = int(bin(raw[1])[2:][-3]) * '-'
+                Lng = int(bin(raw[1])[2:][-2]) * '-'
+                LngH = bin(raw[1])[2:][-1]
+                # Byte 3,4 (Latitude HHMM)
+                LatH = str(int('0' + bin(raw[2])[2:][-4:], 2)) + str(int('0' + bin(raw[2])[2:][-8:-4], 2))
+                LatM = str(int('0' + bin(raw[3])[2:][-4:], 2)) + str(int('0' + bin(raw[3])[2:][-8:-4], 2))
+                # Byte 5,6 (Latitude .MMMM)
+                LatM += '.' + str(int('0' + bin(raw[4])[2:][-4:], 2)) + str(int('0' + bin(raw[4])[2:][-8:-4], 2))
+                LatM += str(int('0' + bin(raw[5])[2:][-4:], 2)) + str(int('0' + bin(raw[5])[2:][-8:-4], 2))
+                sbd['LT'] = float(Lat + str(int(LatH) + float(LatM) / 60))
+                # Byte 7,8 (Longitude HHMM)
+                LngH += str(int('0' + bin(raw[6])[2:][-4:], 2)) + str(int('0' + bin(raw[6])[2:][-8:-4], 2))
+                LngM = str(int('0' + bin(raw[7])[2:][-4:], 2)) + str(int('0' + bin(raw[7])[2:][-8:-4], 2))
+                # Byte 9,10 (Longitude .MMMM)
+                LngM += '.' + str(int('0' + bin(raw[8])[2:][-4:], 2)) + str(int('0' + bin(raw[8])[2:][-8:-4], 2))
+                LngM += str(int('0' + bin(raw[9])[2:][-4:], 2)) + str(int('0' + bin(raw[9])[2:][-8:-4], 2))
+                sbd['LG'] = float(Lng + str(int(LngH) + float(LngM) / 60))
+                if not lat_long_isvalid(sbd['LT'], sbd['LG']):
+                    raise ValueError('Lon/Lat {},{} is not valid.'.format(sbd['LG'], sbd['LT']))
+                if len(raw) == 14:
+                    # Byte 11,12,13,14 is unix time, but local to the device??
+                    # use email timestamp because within 10 secs and fairly accurate
+                    # might have future issues with delayed retransmits
+                    sbd['LOCALTU'] = raw[10]
+                    # Byte 15,16 are speed in 10 m/h
+                    sbd['VL'] = raw[11] * 10
+                    # Byte 17,18 is altitude in m above sea level
+                    sbd['AL'] = raw[12]
+                    # Byte 19,20 is direction in degrees
+                    sbd['DR'] = raw[13]
+                LOGGER.debug(str(sbd))
+            else:
+                LOGGER.warning("Don't know how to read " + force_text(sbd['EQ']) + " - " + force_text(raw))
                 dimap.flag(msgid)
                 return
-        else:
-            LOGGER.warning("Extra stuff")
+        except Exception as e:
+            LOGGER.warning("error: " + force_text(e))
             dimap.flag(msgid)
             return
-        LoggedPoint.parse_sbd(sbd)
-    except Exception as e:
-        LOGGER.error(e)
+    else:
+        LOGGER.warning("Flagging IridiTrak message {}".format(msgid))
         dimap.flag(msgid)
         return
 
+    LoggedPoint.parse_sbd(sbd)
     dimap.delete(msgid)
 
 
 def save_dplus(dimap, queueitem):
     msgid, msg = queueitem
     sbd = {"RAW": msg.get_payload().strip().split("|")}
-    deviceid = sbd["ID"] = int(sbd["RAW"][0])
     try:
         sbd["LT"] = float(sbd["RAW"][4])
         sbd["LG"] = float(sbd["RAW"][5])
