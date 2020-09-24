50ddb7a028003f056829c3dcbc08d9265e30842b
Ashley Felton <ashley@ropable.com>
2020-09-24 14:49:09 +0800

    Remove weather app and pollstations.py.

--- weather/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from weather.views import index, weatherstation, observations_health
-
-
-urlpatterns = [
-    url(r'^$', index, name='weather_index'),
-    url(r'^observations-health/$', observations_health, name='observations_health'),
-    url(r'^station/(?P<station_id>\d+)$', weatherstation, name='weather_station')
-]
