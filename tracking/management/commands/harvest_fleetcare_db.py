42e5556b33d579bd2dc84819db5cde221b3945dd
Rocky Chen <Rocky.Chen@dbca.wa.gov.au>
2021-01-22 14:43:14 +0800

    Add fleetcare reharvesting feature; Parse the timestamp in json file with patterns "dd/mm/yyyy hh:mm:ss" and "mm/dd/yyyy hh:mm:ss" and choose the datetime which is closer to fleetcare json data creation time which is extract from json file name If the selected pattern is not the first pattern, add a row in table 'tracking_loggedpoint_suspicious' for later inspect

--- tracking/management/commands/harvest_fleetcare_db.py
+++ tracking/management/commands/harvest_fleetcare_db.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from tracking.harvest import save_fleetcare_db
 
+import traceback
+
 
 class Command(BaseCommand):
     help = "Runs save_fleetcare_db to harvest points"
@@ -10,6 +12,6 @@ class Command(BaseCommand):
         self.stdout.write("Harvesting Fleetcare tracking data")
         try:
             out = save_fleetcare_db()
-            self.stdout.write(self.style.SUCCESS("Harvested {} from Fleetcare; created {}, updated {}, ignored {}, errors {}".format(*out)))
-        except Exception as e:
-            raise CommandError(e)
+            self.stdout.write(self.style.SUCCESS("Harvested {} from Fleetcare; created {}, updated {}, overwrite {}, errors {}, suspicious {}, skipped {}".format(*out)))
+        except:
+            traceback.print_exc()
