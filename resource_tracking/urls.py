2c4fdc53e91a97f32d82262936ee82ba260258e6
Ashley Felton <ashley@ropable.com>
2021-09-23 07:58:05 +0800

    Remove harvest_tracking_email from views (shouldn't expose this as a view any more).

--- resource_tracking/urls.py
+++ resource_tracking/urls.py
@@ -1,13 +1,11 @@
 from django.urls import include, path
 from django.contrib import admin
 from django.conf import settings
-from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
 
 from resource_tracking.api import v1_api
 from tracking import urls as tracking_urls
 from tracking.admin import tracking_admin_site
-from tracking.harvest import harvest_tracking_email
 from tracking.geojsonviews import geojson_patterns
 
 
@@ -15,7 +13,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('sss_admin/', tracking_admin_site.urls),
     path('api/', include(v1_api.urls)),
-    path('harvest_tracking_email', harvest_tracking_email),
     path('', include(tracking_urls)),
     path('', include(geojson_patterns)),
     path(
