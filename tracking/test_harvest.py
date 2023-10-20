621b6dc6ea72b1b09866e9e66939f65052a45575
Ashley Felton <ashley@ropable.com>
2023-10-20 09:17:34 +0800

    Refactor tracking tracking functions, add unit tests.

---
+++ tracking/test_harvest.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+from tracking.utils import (
+    validate_latitude_longitude,
+    parse_mp70_payload,
+    parse_beam_payload,
+)
+
+
+# MP70 payload with valid data.
+MP70_PAYLOAD_VALID = "\r\nN694470090021038,13.74,-031.99252,+115.88450,0,0,10/18/2023 03:12:45\r\n"
+# MP70 payload with bad data (unable to parse).
+MP70_PAYLOAD_BAD = "\r\nN690540113021035,12.96,foo,bar,-53,0,10/18/2023 03:12:49\r\n"
+# MP70 payload with invalid data (fails validation).
+MP70_PAYLOAD_INVALID = "\r\nN690540113021035,12.96,+000.00000,+000.00000,-53,0,10/18/2023 03:12:49\r\n"
+# Iriditrak payload with valid data.
+IRIDITRAK_PAYLOAD_VALID = b"\x01\xfd3\x12tqa\x901 \x11\xd60e\x00\x00\xbc\x00\x00\x00"
+
+
+class HarvestTestCase(TestCase):
+    """Unit tests to cover the following harvest formats:
+    - Email payloads: Iriditrak, MP70, Spot
+    - DFES API (TODO)
+    - TracPlus API (TODO)
+    """
+
+    def test_validate_latitude_longitude(self):
+        """Test the validate_latitude_longitude function
+        """
+        data = parse_mp70_payload(MP70_PAYLOAD_INVALID)
+        self.assertFalse(validate_latitude_longitude(data["latitude"], data["longitude"]))
+        data = parse_mp70_payload(MP70_PAYLOAD_VALID)
+        self.assertTrue(validate_latitude_longitude(data["latitude"], data["longitude"]))
+
+    def test_parse_mp70_payload(self):
+        """Test the parse_mp70_payload function
+        """
+        self.assertTrue(parse_mp70_payload(MP70_PAYLOAD_VALID))
+        # Invalid data will still parse.
+        self.assertTrue(parse_mp70_payload(MP70_PAYLOAD_INVALID))
+        self.assertFalse(parse_mp70_payload(MP70_PAYLOAD_BAD))
+
+    def test_parse_beam_payload(self):
+        """Test the parse_beam_payload function
+        """
+        self.assertTrue(parse_beam_payload(IRIDITRAK_PAYLOAD_VALID))
---
+++ tracking/utils.py
@@ -0,0 +1,196 @@
+from datetime import datetime
+from email.utils import parsedate
+import pytz
+import struct
+import time
+
+
+UTC = pytz.timezone("UTC")
+
+
+def validate_latitude_longitude(latitude, longitude):
+    """Validate passed-in latitude and longitude values.
+    """
+    # Both latitude and longitude equalling zero is considered invalid.
+    if latitude == 0.0 and longitude == 0.0:
+        return False
+
+    return latitude <= 90 and latitude >= -90 and longitude <= 180 and longitude >= -180
+
+
+def parse_mp70_payload(payload):
+    """Parses a passed-in MP70 email payload. Returns a dict or False.
+
+    MP70 payloads consist of comma-separated data values:
+    Device_ID, Battery_Voltage, Latitude, Longitude, Speed_km/h, Heading, Time_UTC
+    """
+    # First, remove newline characters and split on commas.
+    # If we can't parse the raw payload, return False.
+    try:
+        payload = payload.replace('=\r\n', '').strip().split(",")
+    except:
+        return False
+
+    # Next, cast the payload elements as the correct types.
+    # If we can't cast the types, return False.
+    try:
+        data = {
+            "device_id": payload[0],
+            # We don't care about the second element (battery voltage).
+            "latitude": float(payload[2]),
+            "longitude": float(payload[3]),
+            "velocity": int(payload[4]),
+            "heading": int(payload[5]),
+            "timestamp": time.mktime(datetime.strptime(payload[6], "%m/%d/%Y %H:%M:%S").timetuple()),
+            "type": "mp70",
+        }
+    except:
+        return False
+
+    # Localise the timestamp as UTC.
+    data["timestamp"] = UTC.localize(datetime.fromtimestamp(float(data["timestamp"])))
+
+    return data
+
+
+def parse_spot_message(message):
+    """Parses the passed-in Spot email message. Returns a dict or False.
+    """
+    try:
+        data = {
+            "device_id": message["X-SPOT-Messenger"],
+            "latitude": float(message["X-SPOT-Latitude"]),
+            "longitude": float(message["X-SPOT-Longitude"]),
+            "velocity": 0,
+            "heading": 0,
+            "timestamp": time.mktime(parsedate(message["DATE"])),
+            "type": "spot",
+        }
+    except:
+        return False
+
+    # Localise the timestamp as UTC.
+    data["timestamp"] = UTC.localize(datetime.fromtimestamp(float(data["timestamp"])))
+
+    return data
+
+
+def parse_beam_payload(attachment):
+    """Attempt to parse the binary attachment for tracking data. Returns a dict or False.
+    Reference: https://www.beamcommunications.com/document/342-beam-message-format
+    """
+    beam = dict()
+    raw = struct.unpack("<BBBBBBBBBBIHHH"[: len(attachment) + 1], attachment)
+
+    # Byte 1 Equation byte, use to detect the type of message.
+    eq = raw[0]
+
+    # BEAM 10 byte and 20 byte binary messages
+    if eq in [1, 2, 3, 4, 18, 19, 25, 26]:
+        latitude = int(bin(raw[1])[2:][-3]) * "-"
+        latitude_h = str(int("0" + bin(raw[2])[2:][-4:], 2)) + str(
+            int("0" + bin(raw[2])[2:][-8:-4], 2)
+        )
+        # Byte 3,4 (Latitude HHMM)
+        latitude_m = str(int("0" + bin(raw[3])[2:][-4:], 2)) + str(
+            int("0" + bin(raw[3])[2:][-8:-4], 2)
+        )
+        # Byte 5,6 (Latitude .MMMM)
+        latitude_m += (
+            "."
+            + str(int("0" + bin(raw[4])[2:][-4:], 2))
+            + str(int("0" + bin(raw[4])[2:][-8:-4], 2))
+        )
+        latitude_m += str(int("0" + bin(raw[5])[2:][-4:], 2)) + str(
+            int("0" + bin(raw[5])[2:][-8:-4], 2)
+        )
+
+        beam["latitude"] = float(latitude + str(int(latitude_h) + float(latitude_m) / 60))
+
+        longitude = int(bin(raw[1])[2:][-2]) * "-"
+        longitude_h = bin(raw[1])[2:][-1]
+        # Byte 7,8 (Longitude HHMM)
+        longitude_h += str(int("0" + bin(raw[6])[2:][-4:], 2)) + str(
+            int("0" + bin(raw[6])[2:][-8:-4], 2)
+        )
+        longitude_m = str(int("0" + bin(raw[7])[2:][-4:], 2)) + str(
+            int("0" + bin(raw[7])[2:][-8:-4], 2)
+        )
+        # Byte 9,10 (Longitude .MMMM)
+        longitude_m += (
+            "."
+            + str(int("0" + bin(raw[8])[2:][-4:], 2))
+            + str(int("0" + bin(raw[8])[2:][-8:-4], 2))
+        )
+        longitude_m += str(int("0" + bin(raw[9])[2:][-4:], 2)) + str(
+            int("0" + bin(raw[9])[2:][-8:-4], 2)
+        )
+        beam["longitude"] = float(longitude + str(int(longitude_h) + float(longitude_m) / 60))
+
+        if len(raw) == 14:
+            # Byte 15,16 are speed in 10 m/h
+            beam["velocity"] = raw[11] * 10
+            # Byte 17,18 is altitude in m above sea level
+            beam["altitude"] = raw[12]
+            # Byte 19,20 is direction in degrees
+            beam["heading"] = raw[13]
+    else:
+        return False
+
+    return beam
+
+
+def parse_iriditrak_message(message):
+    """Parses a passed-in Iriditrak email message. Returns a dict or False.
+    """
+    try:
+        data = {
+            "device_id": message["SUBJECT"].replace("SBD Msg From Unit: ", ""),
+            "timestamp": time.mktime(parsedate(message["DATE"])),
+            "type": "iriditrak",
+        }
+
+        # BEAM binary message attachment.
+        attachment = None
+        for part in message.walk():
+            if part.get_content_maintype() != "multipart":
+                attachment = part.get_payload(decode=True)
+
+        # If the attachment is absent or greater than 20 bytes, abort.
+        if not attachment or len(attachment) > 20:
+            return False
+
+        beam = parse_beam_payload(attachment)
+        if not beam:
+            return False
+
+        data = {**data, **beam}
+
+        # Localise the timestamp as UTC.
+        data["timestamp"] = UTC.localize(datetime.fromtimestamp(float(data["timestamp"])))
+
+    except:
+        return False
+
+    return data
+
+
+def parse_dplus_payload(payload):
+    data = {"RAW": payload.strip().split("|")}
+
+    try:
+        data["device_id"] = int(data["RAW"][0])
+        data["latitude"] = float(data["RAW"][4])
+        data["longitude"] = float(data["RAW"][5])
+        data["timestamp"] = time.mktime(datetime.strptime(data["RAW"][1], "%d-%m-%y %H:%M:%S").timetuple())
+        data["velocity"] = int(data["RAW"][6]) * 1000
+        data["heading"] = int(data["RAW"][7])
+        data["altitude"] = int(data["RAW"][9])
+        data["type"] = "dplus"
+
+        # Localise the timestamp as UTC.
+        data["timestamp"] = UTC.localize(datetime.fromtimestamp(float(data["timestamp"])))
+    except:
+        return False
+
+    return data
