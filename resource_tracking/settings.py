a02e0f1548e041c3b75468bc6a2a5680863a836c
Ashley Felton <ashley@ropable.com>
2023-10-25 10:00:35 +0800

    Tweak Sentry env vars.

--- resource_tracking/settings.py
+++ resource_tracking/settings.py
@@ -149,12 +149,15 @@ TASTYPIE_DEFAULT_FORMATS = ['json']
 
 # Sentry settings
 SENTRY_DSN = env('SENTRY_DSN', None)
-if SENTRY_DSN:
+SENTRY_SAMPLE_RATE = env('SENTRY_SAMPLE_RATE', 0.0)  # 0.0 - 1.0
+SENTRY_ENVIRONMENT = env('SENTRY_ENVIRONMENT', None)
+if SENTRY_DSN and SENTRY_ENVIRONMENT:
     import sentry_sdk
 
     sentry_sdk.init(
         dsn=SENTRY_DSN,
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
-        traces_sample_rate=1.0,
+        traces_sample_rate=SENTRY_SAMPLE_RATE,
+        environment=SENTRY_ENVIRONMENT,
     )
