b38040ab082795c678fe7aee2092184c5b8fcbb9
Ashley Felton <ashley@ropable.com>
2024-02-13 09:42:43 +0800

    Easier views import.

--- tracking/urls.py
+++ tracking/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from tracking.views import DevicesView, DeviceHistoryView, DeviceRouteView
+from tracking import views
 
 
 urlpatterns = [
-    path("devices.csv", DevicesView.as_view(format="csv"), name="track_device_csv"),
-    path("devices.geojson", DevicesView.as_view(), name="track_device_geojson"),
-    path("loggedpoint/<int:device_id>.csv", DeviceHistoryView.as_view(format="csv"), name="device_history_view_csv"),
-    path("loggedpoint/<int:device_id>.geojson", DeviceHistoryView.as_view(), name="device_history_view_geojson"),
-    path("route/<int:device_id>.geojson", DeviceRouteView.as_view(), name="device_route_view_geojson"),
+    path("devices.csv", views.DevicesView.as_view(format="csv"), name="track_device_csv"),
+    path("devices.geojson", views.DevicesView.as_view(), name="track_device_geojson"),
+    path("loggedpoint/<int:device_id>.csv", views.DeviceHistoryView.as_view(format="csv"), name="device_history_view_csv"),
+    path("loggedpoint/<int:device_id>.geojson", views.DeviceHistoryView.as_view(), name="device_history_view_geojson"),
+    path("route/<int:device_id>.geojson", views.DeviceRouteView.as_view(), name="device_route_view_geojson"),
 ]
