d8ed41368c4e2372f6236f56293a67e9f883d0fd
Ashley Felton <ashley@ropable.com>
2024-03-11 10:07:21 +0800

    Unit test for parse_iriditrak_message function.

--- tracking/test_harvest.py
+++ tracking/test_harvest.py
@@ -1,12 +1,16 @@
 import csv
 from datetime import datetime, timezone
+import email
 import json
+import os
+from django.conf import settings
 from django.test import TestCase
 
 from tracking.models import Device
 from tracking.utils import (
     validate_latitude_longitude,
     parse_mp70_payload,
+    parse_iriditrak_message,
     parse_beam_payload,
     parse_tracplus_row,
     parse_dfes_feature,
@@ -21,7 +25,9 @@ MP70_PAYLOAD_BAD = "\r\nN690540113021035,12.96,foo,bar,-53,0,10/18/2023 03:12:49
 # MP70 payload with invalid data (fails validation).
 MP70_PAYLOAD_INVALID = "\r\nN690540113021035,12.96,+000.00000,+000.00000,-53,0,10/18/2023 03:12:49\r\n"
 # Iriditrak BEAM payload with valid data.
-IRIDITRAK_PAYLOAD_VALID = b"\x01\xfd3\x12tqa\x901 \x11\xd60e\x00\x00\xbc\x00\x00\x00"
+IRIDITRAK_PAYLOAD_VALID = b"\x03\xadCSB\x00aa\x80\x11,U\xeee]\x1b\x97\x00J\x01"
+# Iriditrak timestamp in the test email message.
+IRIDTRAK_TIMESTAMP = datetime(2024, 3, 11, 0, 50, 1, tzinfo=timezone.utc)
 # TracPlus feed valid payload
 TRAKPLUS_FEED_VALID = """Report ID,Asset Type,Asset Regn,Asset Name,Asset Make,Asset Model,Device ID,Device Serial,Device IMEI,Device Make,Device Model,Transmitted,Received,Latitude,Longitude,Speed,Track,Altitude,Events,GPS Count,DOP,Type of Fix,Message Text,Package,Gateway\r\n1,Aircraft,N293EA Erickson Aero Tanker,,McDonnell Douglas,DC-87,1,L0001,L1001,Flightcell,DZMx,2023-10-30 01:50:44,2023-10-30 01:50:49,44.66921790,-121.149994450,0,0,750,EVT_SCHEDULED,0,1,3D,,,IRIDIUM.SBD.SQS
 """
@@ -92,7 +98,13 @@ class HarvestTestCase(TestCase):
         """Test the parse_beam_payload function
         """
         self.assertTrue(parse_beam_payload(IRIDITRAK_PAYLOAD_VALID))
+
+    def test_parse_iriditrak_message(self):
         # Iriditrak timestamp is parsed from the sent email.
+        iriditrak_email = open(os.path.join(settings.BASE_DIR, "tracking", "iriditrak_test.msg"))
+        message = email.message_from_string(iriditrak_email.read())
+        data = parse_iriditrak_message(message)
+        self.assertEqual(IRIDTRAK_TIMESTAMP, data["timestamp"])
 
     def test_parse_spot_message(self):
         """TODO: test the parse_spot_message function
