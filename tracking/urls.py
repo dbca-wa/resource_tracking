0dc81b0dcf61de72e72d7cba716e79b1908c1954
Ashley Felton <ashley@ropable.com>
2024-06-05 15:13:04 +0800

    Allow filtering on DeviceView.

--- tracking/urls.py
+++ tracking/urls.py
@@ -4,8 +4,8 @@ from tracking import views
 
 urlpatterns = [
     path("map/", views.ResourceMap.as_view(), name="resource_map"),
-    path("devices.csv", views.DevicesView.as_view(format="csv"), name="device_csv"),
-    path("devices.geojson", views.DevicesView.as_view(), name="device_geojson"),
+    path("devices.csv", views.DeviceView.as_view(format="csv"), name="device_csv"),
+    path("devices.geojson", views.DeviceView.as_view(), name="device_geojson"),
     path("loggedpoint/<int:device_id>.csv", views.DeviceHistoryView.as_view(format="csv"), name="device_history_csv"),
     path("loggedpoint/<int:device_id>.geojson", views.DeviceHistoryView.as_view(), name="device_history_geojson"),
     path("route/<int:device_id>.geojson", views.DeviceRouteView.as_view(), name="device_route_geojson"),
