0764d5389e066be707943b8034df22d199491e5f
Ashley Felton <ashley@ropable.com>
2025-02-28 09:23:59 +0800

    Tweak map.js

--- tracking/static/js/map.js
+++ tracking/static/js/map.js
@@ -1,31 +1,29 @@
-"use strict";
+'use strict';
 
 // Parse additional variables from the DOM element.
-const context = JSON.parse(document.getElementById("javascript_context").textContent);
+const context = JSON.parse(document.getElementById('javascript_context').textContent);
 
-const geoserver_wmts_url =
-  context.geoserver_url +
-  "/gwc/service/wmts?service=WMTS&request=GetTile&version=1.0.0&tilematrixset=mercator&tilematrix=mercator:{z}&tilecol={x}&tilerow={y}";
-const geoserver_wmts_url_base = geoserver_wmts_url + "&format=image/jpeg";
-const geoserver_wmts_url_overlay = geoserver_wmts_url + "&format=image/png";
+const geoserver_wmts_url = `${context.geoserver_url}/gwc/service/wmts?service=WMTS&request=GetTile&version=1.0.0&tilematrixset=mercator&tilematrix=mercator:{z}&tilecol={x}&tilerow={y}`;
+const geoserver_wmts_url_base = `${geoserver_wmts_url}&format=image/jpeg`;
+const geoserver_wmts_url_overlay = `${geoserver_wmts_url}&format=image/png`;
 
 // Base layers
-const mapboxStreets = L.tileLayer(geoserver_wmts_url_base + "&layer=dbca:mapbox-streets");
-const landgateOrthomosaic = L.tileLayer(geoserver_wmts_url_base + "&layer=landgate:virtual_mosaic");
-const stateMapBase = L.tileLayer(geoserver_wmts_url_base + "&layer=cddp:state_map_base");
-const openStreetMap = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png");
+const mapboxStreets = L.tileLayer(`${geoserver_wmts_url_base}&layer=dbca:mapbox-streets`);
+const landgateOrthomosaic = L.tileLayer(`${geoserver_wmts_url_base}&layer=landgate:virtual_mosaic`);
+const stateMapBase = L.tileLayer(`${geoserver_wmts_url_base}&layer=cddp:state_map_base`);
+const openStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png');
 
 // Overlay layers
-const dbcaBushfires = L.tileLayer(geoserver_wmts_url_overlay + "&layer=landgate:dbca_going_bushfires_dbca-001", {
+const dbcaBushfires = L.tileLayer(`${geoserver_wmts_url_overlay}&layer=landgate:dbca_going_bushfires_dbca-001`, {
   transparent: true,
   opacity: 0.75,
 });
-const dfesBushfires = L.tileLayer(geoserver_wmts_url_overlay + "&layer=landgate:authorised_fireshape_dfes-032", {
+const dfesBushfires = L.tileLayer(`${geoserver_wmts_url_overlay}&layer=landgate:authorised_fireshape_dfes-032`, {
   transparent: true,
   opacity: 0.75,
 });
-const dbcaRegions = L.tileLayer(geoserver_wmts_url_overlay + "&layer=cddp:kaartdijin-boodja-public_CPT_DBCA_REGIONS");
-const lgaBoundaries = L.tileLayer(geoserver_wmts_url_overlay + "&layer=cddp:local_gov_authority");
+const dbcaRegions = L.tileLayer(`${geoserver_wmts_url_overlay}&layer=cddp:kaartdijin-boodja-public_CPT_DBCA_REGIONS`, { opacity: 0.75 });
+const lgaBoundaries = L.tileLayer(`${geoserver_wmts_url_overlay}&layer=cddp:local_gov_authority`, { opacity: 0.75 });
 
 // Icon classes (note that URLs are injected into the base template.)
 const iconCar = L.icon({
@@ -94,23 +92,23 @@ const iconOther = L.icon({
   iconAnchor: [16, 16],
 });
 
-const toastRefresh = bootstrap.Toast.getOrCreateInstance(document.getElementById("toastRefresh"));
-const toastError = bootstrap.Toast.getOrCreateInstance(document.getElementById("toastError"));
+const toastRefresh = bootstrap.Toast.getOrCreateInstance(document.getElementById('toastRefresh'));
+const toastError = bootstrap.Toast.getOrCreateInstance(document.getElementById('toastError'));
 
 // Define layer groups.
 const baseMaps = {
   OpenStreetMap: openStreetMap,
-  "Mapbox streets": mapboxStreets,
-  "Landgate orthomosaic": landgateOrthomosaic,
-  "State map base 250K": stateMapBase,
+  'Mapbox streets': mapboxStreets,
+  'Landgate orthomosaic': landgateOrthomosaic,
+  'State map base 250K': stateMapBase,
 };
 const overlayMaps = {
-  "DBCA Going Bushfires": dbcaBushfires,
-  "DFES Going Bushfires": dfesBushfires,
-  "DBCA regions": dbcaRegions,
-  "LGA boundaries": lgaBoundaries,
+  'DBCA Going Bushfires': dbcaBushfires,
+  'DFES Going Bushfires': dfesBushfires,
+  'DBCA regions': dbcaRegions,
+  'LGA boundaries': lgaBoundaries,
 };
-const map = L.map("map", {
+const map = L.map('map', {
   center: [-31.96, 115.87],
   zoom: 12,
   minZoom: 4,
@@ -123,4 +121,4 @@ L.control.scale({ maxWidth: 500, imperial: false }).addTo(map);
 // Fullscreen control
 L.control.fullscreen().addTo(map);
 // Link to device list view.
-L.easyButton("fa-solid fa-list", () => window.open(context.device_list_url, "_self"), "Device list", "idDeviceListControl").addTo(map);
+L.easyButton('fa-solid fa-list', () => window.open(context.device_list_url, '_self'), 'Device list', 'idDeviceListControl').addTo(map);
