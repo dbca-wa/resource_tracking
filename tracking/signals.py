fc11c327122249445d40c1f91309f45933aa08c5
Ashley Felton <ashley@ropable.com>
2026-08-20 14:41:37 +0800

    Comment

--- tracking/signals.py
+++ tracking/signals.py
@@ -6,6 +6,8 @@ from django.dispatch import receiver
 @receiver(post_save, sender=User)
 def user_post_save(sender, instance, created, **kwargs):
     # Add users to the 'Edit Resource Tracking Device' group so users can edit Device details
+    # All users have this change permission by default.
+    # Reference: https://dbca.freshservice.com/a/tickets/223528
     if created:
         g, _ = Group.objects.get_or_create(name="Edit Resource Tracking Device")
         instance.groups.add(g)
